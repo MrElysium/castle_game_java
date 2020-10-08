d8c4946aa3a3134dc63e344124114e57ca79ec93
MrElysium <67595580+MrElysium@users.noreply.github.com>
2020-10-08 08:06:41 +0800

    用Hash表来表示Room的方向，而不在用成员变量来表示。

--- src/Room.java
+++ src/Room.java
@@ -1,58 +1,35 @@
 
+import java.util.HashMap;
+
 public class Room {
 	private String description;
-    private Room northExit;
-    private Room southExit;
-    private Room eastExit;
-    private Room westExit;
-      
+    private HashMap<String,Room> exit = new HashMap<String,Room>();
+	  
     public Room(String description) 
     {
         this.description = description;
     }
-
-    public void setExits(Room north, Room east, Room south, Room west) 
-    {
-        if(north != null)
-            northExit = north;
-        if(east != null)
-            eastExit = east;
-        if(south != null)
-            southExit = south;
-        if(west != null)
-            westExit = west;
+    
+    public void setExit(String dir,Room room) {
+    	exit.put(dir, room);    	
     }
+    
+    
 
     public void printOptions(){
     	StringBuilder sb = new StringBuilder();
     	sb.append("choose the direction: ");
-        if(this.northExit != null)
-        	sb.append("north ");
-        if(this.eastExit != null)
-        	sb.append("east ");
-        if(this.southExit != null)
-        	sb.append("south ");
-        if(this.westExit != null)
-        	sb.append("west ");
-        System.out.println(sb.toString());
+    	for(String dir : exit.keySet()) {
+    		sb.append(dir)
+    		.append(" ");
+    	}
+    	System.out.println(sb.toString());
         System.out.println();
     }
     
     public Room nextRoom(String direction) {
-    	Room nextRoom =null;
-    	if(direction.equals("north")) {
-            nextRoom = this.northExit;
-        }
-        if(direction.equals("east")) {
-            nextRoom = this.eastExit;
-        }
-        if(direction.equals("south")) {
-            nextRoom = this.southExit;
-        }
-        if(direction.equals("west")) {
-            nextRoom = this.westExit;
-        }
-        
+    	Room nextRoom =exit.get(direction);;
+    	        
 		return nextRoom;   	
         
     }
